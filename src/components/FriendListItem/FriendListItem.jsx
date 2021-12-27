852bbeb0047a154c1d3032d96d0025493aa577dd
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-12-27 01:29:09 +0100

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,9 @@
 const FriendListItem = ({ avatar, name, isOnline }) => (
-  <li class="item">
+  <>
     <span class="status">{isOnline}</span>
     <img class="avatar" src={avatar} alt="User avatar" width="48" />
     <p class="name">{name}</p>
-  </li>
+  </>
 );
 
 export default FriendListItem;
