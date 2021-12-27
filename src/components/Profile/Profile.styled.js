2a59a59e5bbfda447f9179f17dda6e7f52dc6928
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-12-27 01:29:20 +0100

    Create Profile.styled.js

---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled'
+
+export const ProfileContainer = styled.div`
+width: 300px;
+margin: 0 auto;
+margin-bottom: 20px;
+background-color: #fff;
+border: 1px solid #cccccc;
+box-shadow: 8px 10px 14px 0px rgba(34, 60, 80, 0.2)`
+
+  export const Avatar = styled.img`
+  width: 160px;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 50%;
+  overflow: hidden;`
+
+  export const Name = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1.179;`
+
+  export const Text = styled.p`
+  color: #7d8687`
+
+  export const Stats = styled.ul`
+  display: flex;
+  justify-content: center;
+  `
+export const StatsItem = styled.li`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 20px;
+width: 100%;
+background-color: #e5e5e5;
+border: 1px solid #cccccc;
+`
+export const LabelEl = styled.span`
+color: #7d8687
+`
+
+export const QuantityEl = styled.span`
+font-size: 20px;
+font-weight: 700;
+line-height: 1.179
+`
