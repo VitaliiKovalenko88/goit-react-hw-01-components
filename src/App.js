be19784b503e31be226bbb054ab261835e7e83d4
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-02 20:56:08 +0100

    add information for app about user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Profile from "./components/Profile";
+import user from "./data/user";
 
 function App() {
-  return <Profile/>;
+  return <Profile 
+  username={user.name}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.avatar}
+  />;
 }
 export default App;
