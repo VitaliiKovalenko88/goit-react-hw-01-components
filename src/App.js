9e8797090e390018e02c144bc0976ec9022f37ef
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-03 22:09:16 +0100

    create component Statistik with data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
 import user from "./data/user/user.json";
+import data from "./data/data/data.json"
 
 function App() {
-  return <Profile 
+  console.log()
+  return <>
+  <Profile 
   username={user.name}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
   />;
+  <Statistics
+  title='Upload stats'
+  stats={data}
+  />
+  </>
 }
 export default App;
---
+++ src/components/Statistics/index.js
@@ -0,0 +1,18 @@
+
+
+const Statistics = (stats) =>{
+return (
+<section class="statistics">
+<h2 class="title">Upload stats</h2>
+
+<ul class="stat-list">
+  <li class="item">
+    <span class="label">.docx</span>
+    <span class="percentage">4%</span>
+  </li>
+</ul>
+</section>)
+};
+
+
+export default Statistics;
