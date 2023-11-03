8e7a8ab799ea74b36ac37e7adad346f8da8a1eb1
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-03 22:45:24 +0100

    completing second task

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -4,13 +4,17 @@ const Statistics = ({title, stats} ) =>{
   
 return (
 <section class="statistics">
-{title?<h2 class="title">{title}</h2>:null}
+  {title 
+  ? 
+  <h2 class="title">{title}</h2>
+  :
+null}
 
 <ul class="stat-list">
   {stats.map(stat=>(
     <li class="item" key={stat.id}>
-    <span class="label"></span>
-    <span class="percentage"></span>
+    <span class="label">{stat.label}</span>
+    <span class="percentage"> {stat.percentage}</span>
   </li>
   ) )}
 </ul>
