990d2d4b6decb0e82d9cb47802dee9b636a4da5a
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-04 15:21:59 +0100

    change file name in components

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,17 @@
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <li class="item" key={id}>
+            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
---
+++ src/components/FriendListItem/FriendListItem.js
@@ -0,0 +1,11 @@
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <>
+      <span class="status">{isOnline}</span>
+      <img class="avatar" src={avatar} alt="User avatar" width="48" />
+      <p class="name">{name}</p>
+    </>
+  );
+};
+
+export default FriendListItem;
